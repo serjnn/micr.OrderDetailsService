b212e7c9df88a869ec769e452a7b9bd9a29db616
sergey <sersh200606@gmail.com>
2026-03-17 16:22:24 +0300

    fix: flyway security

--- src/test/java/com/serjnn/OrderDetailsService/AbstractIntegrationTest.java
+++ src/test/java/com/serjnn/OrderDetailsService/AbstractIntegrationTest.java
@@ -2,6 +2,7 @@ package com.serjnn.OrderDetailsService;
 
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -10,24 +11,13 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 
-@SpringBootTest(classes = OrderDetailsServiceApplication.class)
+@SpringBootTest()
 @AutoConfigureMockMvc
 @Testcontainers
 public abstract class AbstractIntegrationTest {
     @Container
+    @ServiceConnection
     static PostgreSQLContainer<?> postgres =
-            new PostgreSQLContainer<>(DockerImageName.parse("postgres:15-alpine"));
+            new PostgreSQLContainer<>("postgres:15-alpine");
 
-
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
-        registry.add("spring.flyway.url", postgres::getJdbcUrl);
-        registry.add("spring.flyway.user", postgres::getUsername);
-        registry.add("spring.flyway.password", postgres::getPassword);
-        registry.add("spring.flyway.enabled", () -> "true");
-        registry.add("eureka.client.enabled", () -> "false");
-    }
 }
